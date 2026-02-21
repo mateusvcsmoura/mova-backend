41192109afa78e8f697ba1cc2d3431aa85843aef
mateusvcsmoura <mateusvcsmoura@gmail.com>
2026-02-21 18:21:40 -0300

    feat: adicionando a pool basica do sql

---
+++ src/database/connection.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+});
+
