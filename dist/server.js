a909772f09f1ebccbfa1be9fd12f86d73fdf9b05
z620893 <z620893.terceiros@claro.com.br>
2026-02-21 13:58:16 -0300

    feat: setup inicial do repo (typescript + express)

---
+++ dist/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+    return res.json({ message: "API rodando 🚀" });
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
