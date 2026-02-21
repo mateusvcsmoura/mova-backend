69d6fd77ae73b018833109e94ddf296238a57dbb
mateusvcsmoura <mateusvcsmoura@gmail.com>
2026-02-21 20:34:53 -0300

    fix: organizando a ordem das tabelas geradas para evitar conflito entre locatario x deficiencia

--- src/database/migrations/001_create_tables.sql
+++ src/database/migrations/001_create_tables.sql
@@ -6,6 +6,11 @@ CREATE TABLE IF NOT EXISTS Conta (
     senha_hash VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS Deficiencia (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    descricao VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS Locatario (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     cpf VARCHAR(14) UNIQUE NOT NULL,
@@ -28,9 +33,4 @@ CREATE TABLE IF NOT EXISTS Locador (
         FOREIGN KEY (id)
         REFERENCES Conta(id)
         ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS Deficiencia (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    descricao VARCHAR(255) NOT NULL
 );
