38915ffe0d0c4e19d2531e418f4ba108b59f651f
mateusvcsmoura <mateusvcsmoura@gmail.com>
2026-02-22 20:41:08 -0300

    feat: adicionando o repository de Conta

---
+++ src/repositories/conta.repository.ts
@@ -0,0 +1,78 @@
+import { pool } from "../database/pool.js";
+
+export class ContaRepository {
+  async findAll() {
+    const result = await pool.query(
+      "SELECT id, nome, email, telefone, criada_em FROM public.conta",
+    );
+
+    return result.rows;
+  }
+
+  async findByEmail(email: string) {
+    const result = await pool.query(
+      "SELECT * FROM public.conta WHERE email = $1",
+      [email],
+    );
+
+    return result.rows[0] || null;
+  }
+
+  async findById(id: string) {
+    const result = await pool.query(
+      "SELECT * FROM public.conta WHERE id = $1",
+      [id],
+    );
+
+    return result.rows[0] || null;
+  }
+
+  async create(data: {
+    nome: string;
+    email: string;
+    telefone?: string;
+    senha_hash: string;
+  }) {
+    const result = await pool.query(
+      `INSERT INTO public.conta
+                (nome, email, telefone, senha_hash)
+                VALUES ($1, $2, $3, $4)
+                RETURNING *`,
+      [data.nome, data.email, data.telefone ?? null, data.senha_hash],
+    );
+
+    return result.rows[0];
+  }
+
+  async update(
+    id: string,
+    data: {
+      nome?: string;
+      telefone?: string;
+    },
+  ) {
+    const result = await pool.query(
+      `UPDATE public.conta
+     SET nome = COALESCE($1, nome),
+         telefone = COALESCE($2, telefone)
+     WHERE id = $3
+     RETURNING *`,
+      [data.nome ?? null, data.telefone ?? null, id],
+    );
+
+    return result.rows[0] || null;
+  }
+
+  async updatePassword(id: string, senha_hash: string) {
+    await pool.query(
+      `UPDATE public.conta
+     SET senha_hash = $1
+     WHERE id = $2`,
+      [senha_hash, id],
+    );
+  }
+
+  async delete(id: string) {
+    await pool.query(`DELETE FROM public.conta WHERE id = $1`, [id]);
+  }
+}
