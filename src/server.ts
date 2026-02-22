f10fa63469ced21bb2e8151629532a215cfb8a81
mateusvcsmoura <mateusvcsmoura@gmail.com>
2026-02-22 20:42:03 -0300

    fix: arrumando os server para aceitar o middleware de erro + routers

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,21 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import { testConnection } from "./database/utils/connectionTest.js";
+import { errorHandler } from "./middlewares/error-handler.js";
+import { basicRouter } from "./routes/basic/basic.js";
+import { contaRouter } from "./routes/conta/conta.js";
 
+const PORT = Number(process.env.SERVER_PORT);
 const app = express();
 
 app.use(express.json());
+app.use(errorHandler);
 
-app.get("/", (req, res) => {
-  return res.json({ message: "API online" });
-});
-
-const PORT = Number(process.env.SERVER_PORT);
+app.use("/basic", basicRouter);
+app.use("/conta", contaRouter);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
   testConnection();
 });
-
