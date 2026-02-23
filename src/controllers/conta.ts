93edde538651a34cbdd52bb1554987e3fca35398
mateusvcsmoura <mateusvcsmoura@gmail.com>
2026-02-23 16:27:14 -0300

    feat: adicionando controller de conta

---
+++ src/controllers/conta.ts
@@ -0,0 +1,52 @@
+import { Handler, NextFunction } from "express";
+import { ContaService } from "../services/conta.js";
+import { HttpError } from "../errors/HttpError.js";
+
+export class ContaController {
+  constructor(private readonly contaService: ContaService) {}
+
+  index: Handler = async (req, res, next: NextFunction) => {
+    try {
+      const contas = await this.contaService.findAll();
+
+      return res.status(200).json({ result: contas });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  findByEmail: Handler = async (req, res, next: NextFunction) => {
+    if (!req.query) throw new HttpError(400, "Parâmetros de consulta ausentes");
+
+    try {
+      const { email } = req.query;
+
+      if (!email || typeof email !== "string") {
+        throw new HttpError(400, "Email inválido ou não informado");
+      }
+      const conta = await this.contaService.findByEmail(email);
+
+      return res.status(200).json({ result: conta });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  findById: Handler = async (req, res, next: NextFunction) => {
+    if (!req.params) throw new HttpError(400, "Parâmetros ausentes");
+
+    try {
+      const { id } = req.params;
+
+      if (!id || typeof id !== "string") {
+        throw new HttpError(400, "ID inválido ou não informado");
+      }
+
+      const conta = await this.contaService.findById(id);
+
+      return res.status(200).json({ result: conta });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
