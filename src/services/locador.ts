2b29be2ea452c833939016074d6bbce6f0ce98f5
mateusvcsmoura <mateusvcsmoura@gmail.com>
2026-02-26 11:04:54 -0300

    feat: adicionando metodos faltantes de service do locador

--- src/services/locador.ts
+++ src/services/locador.ts
@@ -1,4 +1,8 @@
-import { CreateLocadorRequest } from "../repositories/contracts/locador.contract.js";
+import { HttpError } from "../errors/HttpError.js";
+import {
+  CreateLocadorRequest,
+  UpdateLocadorRequest,
+} from "../repositories/contracts/locador.contract.js";
 import { LocadorRepository } from "../repositories/locador.repository.js";
 
 export class LocadorService {
@@ -8,7 +12,7 @@ export class LocadorService {
     return await this.locadorRepository.findAll();
   };
 
-  findById = async (id: number) => {
+  findById = async (id: string) => {
     return await this.locadorRepository.findById(id);
   };
 
@@ -16,11 +20,38 @@ export class LocadorService {
     return await this.locadorRepository.findByCnpj(cnpj);
   };
 
-  findByName = async (nome: string) => {
-    return await this.locadorRepository.findByName(nome);
+  findByEmpresa = async (empresa: string) => {
+    return await this.locadorRepository.findByEmpresa(empresa);
   };
 
   create = async (data: CreateLocadorRequest) => {
+    const existingLocador =
+      (await this.locadorRepository.findByCnpj(data.cnpj)) ||
+      (await this.locadorRepository.findByEmpresa(data.empresa));
+
+    if (existingLocador) {
+      throw new HttpError(409, "Locador com este CNPJ ou empresa já existe");
+    }
+
     return await this.locadorRepository.create(data);
+  };
+
+  update = async (id: string, data: UpdateLocadorRequest) => {
+    const existingLocador = await this.locadorRepository.findById(id);
+
+    if (!existingLocador) {
+      throw new HttpError(404, "Locador não encontrado");
+    }
+
+    return await this.locadorRepository.update(id, data);
+  };
+
+  delete = async (id: string) => {
+    const existingLocador = await this.locadorRepository.findById(id);
+
+    if (!existingLocador) {
+      throw new HttpError(404, "Locador não encontrado");
+    }
+    return await this.locadorRepository.delete(id);
   };
 }
