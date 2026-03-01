8c7784c2e9560b80e94b39a649abc2879ad0ad16
mateusvcsmoura <mateusvcsmoura@gmail.com>
2026-03-01 12:35:47 -0300

    fix: corrigindo rotas basicas

--- src/routes/basic/basic.ts
+++ src/routes/basic/basic.ts
@@ -1,17 +1,9 @@
 import { Router } from "express";
-import { ContaRepository } from "../../repositories/conta.repository.js";
 
 const basicRouter = Router();
 
-basicRouter.get("/", (req, res) => {
+basicRouter.get("/status", (req, res) => {
   return res.json({ message: "API online" });
 });
 
-basicRouter.get("/testUser", async (req, res) => {
-  const contaRepo = new ContaRepository();
-  const result = await contaRepo.findByEmail('lee.chaeryeong@kr.com')
-  return res.json({ result });
-});
-
 export { basicRouter };
-
