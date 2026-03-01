7f2cada441a131f7e683c91888548b16e1d412ba
mateusvcsmoura <mateusvcsmoura@gmail.com>
2026-03-01 12:12:16 -0300

    feat: adicionando service de locatario

---
+++ src/services/locatario.ts
@@ -0,0 +1,56 @@
+import { CreateLocatarioRequest } from "./../repositories/contracts/locatario.contract";
+import { LocatarioRepository } from "../repositories/locatario.repository.js";
+import { HttpError } from "../errors/HttpError.js";
+import { UpdateLocatarioRequest } from "../repositories/contracts/locatario.contract.js";
+
+export class LocatarioService {
+  constructor(private readonly locatarioRepository: LocatarioRepository) {}
+
+  findAll = async () => {
+    return await this.locatarioRepository.findAll();
+  };
+
+  findById = async (id: string) => {
+    return await this.locatarioRepository.findById(id);
+  };
+
+  findByCpf = async (cpf: string) => {
+    return await this.locatarioRepository.findByCpf(cpf);
+  };
+
+  findByCnh = async (cnh: string) => {
+    return await this.locatarioRepository.findByCnh(cnh);
+  };
+
+  create = async (data: CreateLocatarioRequest) => {
+    const existingLocatario =
+      (await this.locatarioRepository.findByCpf(data.cpf)) ||
+      (await this.locatarioRepository.findByCnh(data.cnh));
+
+    if (existingLocatario) {
+      throw new HttpError(409, "Locatário com este CPF ou CNH já existe");
+    }
+
+    return await this.locatarioRepository.create(data);
+  };
+
+  update = async (id: string, data: UpdateLocatarioRequest) => {
+    const existingLocatario = await this.locatarioRepository.findById(id);
+
+    if (!existingLocatario) {
+      throw new HttpError(404, "Locatário não encontrado");
+    }
+
+    return await this.locatarioRepository.update(id, data);
+  };
+
+  delete = async (id: string) => {
+    const existingLocatario = await this.locatarioRepository.findById(id);
+
+    if (!existingLocatario) {
+      throw new HttpError(404, "Locatário não encontrado");
+    }
+
+    return await this.locatarioRepository.delete(id);
+  };
+}
