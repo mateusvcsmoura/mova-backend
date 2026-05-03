bb4c91d3331cda200079ba01a6f9512daa640465
mateusvcsmoura <mateusvcsmoura@gmail.com>
2026-05-03 15:30:29 -0300

    fix: adaptando prisma client para receber o banco de test

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,14 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
+const isTest = process.env.NODE_ENV === "test";
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DIRECT_URL"),
+    url: isTest ? env("DIRECT_URL_TEST") : env("DIRECT_URL"),
   },
 });
--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -2,8 +2,12 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { env } from "../config/env.js";
 
+const isTest = process.env.NODE_ENV === "test";
+
+const connectionString = isTest ? env.DATABASE_URL_TEST : env.DATABASE_URL;
+
 const adapter = new PrismaPg({
-  connectionString: env.DATABASE_URL,
+  connectionString,
 });
 
 const prisma = new PrismaClient({
