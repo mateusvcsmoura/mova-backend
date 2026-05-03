0e27611948cbf8262aeb3e705a5dd2ced7a378fd
mateusvcsmoura <mateusvcsmoura@gmail.com>
2026-05-03 15:31:18 -0300

    feat: testando post de auth

---
+++ test/conta/conta.test.ts
@@ -0,0 +1,30 @@
+import request from "supertest";
+import { app } from "../../src/app";
+import { describe, it, expect } from "vitest";
+
+describe("POST /auth/register", () => {
+  it("deve criar um usuário", async () => {
+    const data = {
+      nome: "Jarvan IV",
+      email: "jarvan.iv@lol.com",
+      senha: "StrongPassword#123",
+      telefone: "11987654321",
+      cargo: "LOCATARIO",
+      cep: "12345-678",
+      endereco: "Rua dos Campeões, 123",
+    };
+
+    const response = await request(app)
+      .post("/api/conta/auth/register")
+      .send(data);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("result");
+    expect(response.body.result).toHaveProperty("conta");
+    expect(response.body.result).toHaveProperty("token");
+    expect(response.body.result.conta).toHaveProperty("id");
+    expect(response.body.result.conta.email).toBe("jarvan.iv@lol.com");
+    expect(response.body.result.conta).toHaveProperty("criadaEm");
+    expect(response.body.result.conta).not.toHaveProperty("senhaHash");
+  });
+});
