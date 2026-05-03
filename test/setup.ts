2a68991ee7bb32050dfa4184bfd91aa1f4434b55
mateusvcsmoura <mateusvcsmoura@gmail.com>
2026-05-03 15:31:05 -0300

    feat: adicionando configuracoes do vitest

--- test/setup.ts
+++ test/setup.ts
@@ -2,5 +2,6 @@ import { beforeEach } from "vitest";
 import { prisma } from "../src/database/prisma";
 
 beforeEach(async () => {
+  await prisma.reserva.deleteMany();
   await prisma.conta.deleteMany();
 });
--- vitest.config.js
+++ vitest.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vitest/config";
-import dotenv from "dotenv";
-
-dotenv.config({ path: ".env.test" });
 
 export default defineConfig({
   test: {
     globals: true,
     environment: "node",
-    setupFiles: "./tests/setup.ts",
+    setupFiles: "./test/setup.ts",
+    exclude: ["dist/**", "node_modules"],
   },
 });
